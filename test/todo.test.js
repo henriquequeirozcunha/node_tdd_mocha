312ddfeb9c79618f89b176714be6509fd384079b
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-01-20 22:14:41 -0300

    test: ensure Todo returns invalid when creating an object with no text

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,6 +1,33 @@
 const { describe, it, before } = require('mocha')
 const { expect } = require('chai')
+const Todo = require('../src/todo')
+
+const makeFakeData = () => ({
+    text: 'any_text',
+    when: new Date('2020-12-01')
+})
+
+// const makeSut = () => {
+//     const sut = new Todo(makeFakeData())
+//     return {
+//         sut
+//     }
+// }
+// const { sut } = makeSut()
+// const result = sut.isValid()
 
 describe('todo', () => {
+    describe('#isValid', () => {
+        it('should return invalid when creating an object with no text', () => {
+            const data = {
+                text: '',
+                when: new Date('2020-12-01')
+            }
+
+            const todo = new Todo(data)
+            const result = todo.isValid()
+            expect(result).to.be.not.ok
+        })
+    })
     
 })
