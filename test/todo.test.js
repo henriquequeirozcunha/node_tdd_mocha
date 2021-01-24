ea2e7a2e7f4f7d6c8e1afa886feabf4e42fa796f
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-01-24 09:57:09 -0300

    test: ensure Todo has id, text, when and status on success

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,6 +1,7 @@
 const { describe, it, before } = require('mocha')
 const { expect } = require('chai')
 const Todo = require('../src/todo')
+const { createSandbox } = require('sinon')
 
 const makeFakeData = () => ({
     text: 'any_text',
@@ -17,6 +18,13 @@ const makeFakeData = () => ({
 // const result = sut.isValid()
 
 describe('todo', () => {
+    let sandBox
+    before(() => {
+        sandBox = new createSandbox()
+    });
+    afterEach(() => {
+        sandBox.restore()
+    })
     describe('#isValid', () => {
         it('should return invalid when creating an object with no text', () => {
             const data = {
@@ -39,9 +47,22 @@ describe('todo', () => {
             expect(result).to.be.not.ok
         })
         it('should have "id", "text", "when" and "status" on success', () => {
+            const expectedId = '000001'
+            const uuid = require('uuid')
+            const fakeUUID = sandBox.fake.returns(expectedId)
+            sandBox.replace(uuid, 'v4', fakeUUID)
+            
             const todo = new Todo(makeFakeData())
             const result = todo.isValid()
             expect(result).to.be.ok
+            expect(uuid.v4.calledOnce).to.be.ok
+
+            const expectedResult = {
+                ...todo,
+                status: '',
+                id: expectedId
+            }
+            expect(todo).to.be.deep.equal(expectedResult)
         })
     })
     
